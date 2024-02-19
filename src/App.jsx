8c55cb748b776dc0adc89f8974e405244c2d90c4
Ripon Mondal <riponmondal149@gmail.com>
2024-02-19 17:52:22 +0600

    layout and make react responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Header from './components/Header/Header'
-import Blogs from './components/Blog/Blogs'
+import Blogs from './components/Blogs/Blogs'
+import BookMarks from './components/BookMarks/BookMarks'
 
 function App() {
 
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <BookMarks></BookMarks>
+      </div>
     </>
   )
 }
