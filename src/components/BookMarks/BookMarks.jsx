e76b17a1e7f70631e9c5c4c79097cc744fd61019
Ripon Mondal <riponmondal149@gmail.com>
2024-02-20 22:12:34 +0600

    complete the project

--- src/components/BookMarks/BookMarks.jsx
+++ src/components/BookMarks/BookMarks.jsx
@@ -3,14 +3,16 @@ import Bookmark from '../Bookmark/Bookmark';
 
 const BookMarks = ({ bookmarks, readingTime }) => {
     return (
-        <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4">
-            <div>
-                <h3 className="text-4xl">Reading Time: {readingTime}</h3>
+        <div className="md:w-1/3 ml-4 mt-2 pt-4">
+            <div className='bg-sky-200 p-7 rounded-lg mb-6'>
+                <h3 className="text-4xl text-center">Reading Time: {readingTime}</h3>
             </div>
-            <h2 className='text-3xl text-center'>Bookmarked Blogs: {bookmarks.length}</h2>
-            {
-                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
-            }
+            <div className='bg-gray-300 p-7 rounded-lg'>
+                <h2 className='text-3xl text-center'>Bookmarked Blogs: {bookmarks.length}</h2>
+                {
+                    bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+                }
+            </div>
         </div>
     );
 };
